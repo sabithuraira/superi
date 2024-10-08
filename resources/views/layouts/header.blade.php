a70c43a7a5b7de877c664d04690ff63f3a21fd8b
Sabit Huraira <sabitzhabit@gmail.com>
2024-10-08 10:19:12 +0700

    add theme

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,55 @@
+
+    <nav class="navbar navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-btn">
+                <button type="button" class="btn-toggle-offcanvas"><i class="lnr lnr-menu fa fa-bars"></i></button>
+            </div>
+
+            <div class="navbar-brand">
+                <a href="#">
+                    <img src="{!! asset('lucid/assets/images/bps-sumsel.png') !!}" alt="Lucid Logo" style="width:40px" class="img-responsive logo">
+                </a>   
+            </div>
+            
+            <div class="navbar-right">               
+                <div id="navbar-menu">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                            <a href="javascript:void(0);" class="dropdown-toggle icon-menu" data-toggle="dropdown">
+                                <i class="icon-book-open"></i>
+                            </a>
+                            <ul class="dropdown-menu notifications">
+                                <li class="header"><strong>Unduh Buku Panduan</strong></li>
+                                <li>
+                                    <a href="https://laci.bps.go.id/s/E1hMNODEMVyftUR"> <i class="icon-doc"></i> <span>Panduan Modul SKP, CKP, Penilaian & WFH</span></a>
+                                </li>                               
+                                <li>
+                                    <a href="https://laci.bps.go.id/s/7q6CAaFDjTDk4nB"> <i class="icon-doc"></i> <span>Panduan Modul Barang Persediaan</span></a>
+                                </li>
+                                 <li>
+                                    <a href="https://laci.bps.go.id/s/njTCSRMqiFm4IHV"> <i class="icon-doc"></i> <span>Panduan Modul Surat Menyurat</span></a>
+                                </li>
+                                <li>
+                                    <a href="https://laci.bps.go.id/s/WimneDzApQTyq0V"> <i class="icon-doc"></i> <span>Panduan Modul Surat Tugas</span></a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>#KaloBukanKitaSiapaLagi</li>
+                        <li>
+                            <a href="{{ url('hai') }}">
+                                <img src="{!! asset('lucid/assets/images/logo_sp2020.png') !!}" alt="SP2020 Logo" style="width:70px" class="img-responsive logo">
+                            </a>   
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UC-9WQN-5cNTVTGDF8Rkiu6A" class="icon-menu"><i class="fa fa-youtube-play"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/bpssumsel" class="icon-menu"><i class="fa fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
+    </nav>
